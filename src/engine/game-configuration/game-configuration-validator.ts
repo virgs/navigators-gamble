5a37caa0a10b783c96a9fedc9de279ab38baab6a
Guilherme Moraes <guilherme.moraes@outlook.com>
2025-04-17 11:46:29 -0700

    feat: implement game configuration validation for players, cards, vertices, and edges

--- src/engine/game-configuration/game-configuration-validator.ts
+++ src/engine/game-configuration/game-configuration-validator.ts
@@ -2,6 +2,7 @@ import { doLinesIntersect } from '../../math/line'
 import { SerializableVertix } from '../board/serializable-board'
 import { directions } from '../directions'
 import { GameConfiguration } from './game-configuration'
+import { PlayerType } from './player-type'
 
 const isVertixOutOfBound = (vertix: SerializableVertix) => {
     return vertix.position.x < 0 || vertix.position.x > 1 || vertix.position.y < 0 || vertix.position.y > 1
@@ -34,6 +35,25 @@ export const gameConfigurationLimits = {
     },
 }
 
+type Limits = {
+    min: number
+    max: number
+    step: number
+}
+
+const respectLimits = (value: number, limit: Limits): boolean => {
+    if (value < limit.min) {
+        return false
+    }
+    if (value > limit.max) {
+        return false
+    }
+    if ((value - limit.min) % limit.step !== 0) {
+        return false
+    }
+    return true
+}
+
 export type ValidationResult = {
     valid: boolean
     errors: string[]
@@ -47,8 +67,58 @@ export class GameConfigurationValidator {
     }
 
     public validate(): ValidationResult {
-        const vertices: Readonly<SerializableVertix[]> = this.gameConfiguration.board.vertices
         const errors: string[] = []
+
+        errors.push(...this.validatePlayers())
+        errors.push(...this.validateCards())
+        errors.push(...this.validateVertices())
+        errors.push(...this.validateEdges())
+
+        return {
+            valid: errors.length === 0,
+            errors: errors,
+        }
+    }
+
+    private validatePlayers(): string[] {
+        const errors: string[] = []
+        const aiPlayers = this.gameConfiguration.players.filter(
+            (player) => player.type === PlayerType.ARTIFICIAL_INTELLIGENCE
+        ).length
+
+        if (aiPlayers > 2) {
+            errors.push('Maximum 2 AI players allowed.')
+        }
+        if (this.gameConfiguration.players.length !== 2) {
+            errors.push('The game must have 2 players.')
+        }
+
+        return errors
+    }
+
+    private validateCards(): string[] {
+        const errors: string[] = []
+
+        if (!respectLimits(this.gameConfiguration.cardsPerDirection, gameConfigurationLimits.cardsPerDirection)) {
+            errors.push(
+                `Cards per direction must be between ${gameConfigurationLimits.cardsPerDirection.min} and ${gameConfigurationLimits.cardsPerDirection.max} and a multiple of ${gameConfigurationLimits.cardsPerDirection.step}.`
+            )
+        }
+        if (
+            !respectLimits(this.gameConfiguration.initialCardsPerPlayer, gameConfigurationLimits.initialCardsPerPlayer)
+        ) {
+            errors.push(
+                `Initial cards per player must be between ${gameConfigurationLimits.initialCardsPerPlayer.min} and ${gameConfigurationLimits.initialCardsPerPlayer.max} and a multiple of ${gameConfigurationLimits.initialCardsPerPlayer.step}.`
+            )
+        }
+
+        return errors
+    }
+
+    private validateVertices(): string[] {
+        const errors: string[] = []
+        const vertices: Readonly<SerializableVertix[]> = this.gameConfiguration.board.vertices
+
         if (vertices.length >= this.gameConfiguration.cardsPerDirection * directions.length) {
             errors.push(
                 `Board has too many vertix to game configuration '${vertices.length}'. Max allowed '${
@@ -57,7 +127,7 @@ export class GameConfigurationValidator {
             )
         }
         if (vertices.length < gameConfigurationLimits.board.vertices.min) {
-            errors.push('Not enough vertices. Minimum 6 vertices required.')
+            errors.push('Minimum 6 vertices required.')
         }
         if (!this.gameConfiguration.levelName) {
             errors.push('Level name is required.')
@@ -69,6 +139,13 @@ export class GameConfigurationValidator {
             errors.push('Some vertices are out of bounds.')
         }
 
+        return errors
+    }
+
+    private validateEdges(): string[] {
+        const errors: string[] = []
+        const vertices: Readonly<SerializableVertix[]> = this.gameConfiguration.board.vertices
+
         const edges = vertices
             .flatMap((vertix) =>
                 vertix.linkedVertices.map((linkedVertixId) => ({
@@ -79,7 +156,6 @@ export class GameConfigurationValidator {
             )
             .filter((edge) => edge.end !== undefined)
 
-        // differents edges intersect
         if (
             edges.some((edge1, index) =>
                 edges
@@ -95,11 +171,7 @@ export class GameConfigurationValidator {
             errors.push('Some edges intersect.')
         }
 
-        if (this.gameConfiguration)
-            return {
-                valid: errors.length === 0,
-                errors: errors,
-            }
+        return errors
     }
 
     private numberOfLinksOfAVertix(vertex: SerializableVertix): number {
