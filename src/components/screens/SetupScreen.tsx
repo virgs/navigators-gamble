3c0f6f1b9bbb6d778dc4152932026bac800f591e
Guilherme Moraes <guilherme.moraes@outlook.com>
2025-04-21 21:16:17 -0700

    feat: implement setup screen styles and layout, enhance level selection functionality

--- src/components/screens/SetupScreen.tsx
+++ src/components/screens/SetupScreen.tsx
@@ -1,65 +1,75 @@
-import { ReactNode } from 'react';
+import { ReactNode, use, useState } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
-
-import combined from "../../assets/combined.png";
-import level from '../../assets/levels/custom.json?raw';
 import title from "../../assets/title.png";
 import { GameConfiguration } from '../../engine/game-configuration/game-configuration';
+import './SetupScreen.scss';
+
+const filenames: Record<string, GameConfiguration> = import.meta.glob(`../../assets/levels/*.json`, {
+    eager: true,
+    import: 'default'
+});
+
+const levels = Object.entries(filenames).map(([, config]) => config)
+
 
 export const SetupScreen = (props: { onStartButton: (config: GameConfiguration) => void, onLevelEditorButton: () => void }): ReactNode => {
-    const levelConfig = JSON.parse(level) as GameConfiguration;
+    const [selectedLevelIndex, setSelectedLevelIndex] = useState<number>(0)
 
     return (
-        <Row className='h-100 g-0 justify-content-center align-items-start'>
-            <Col xs={12} sm={5} lg={12} style={{
-                height: 'min(85%, 400px)',
-                maxHeight: 'min(85%, 400px)',
-                textAlign: 'center',
-                marginTop: '40px',
-                background: `url(${combined})`,
-                backgroundSize: 'contain',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-
-            }} >
-                <Image style={{ marginTop: '-30px', width: 'max(30%, 250px)' }} src={title} fluid />
+        <Row className='setup-screen-container g-0'>
+            <Col xs={12} sm={5} lg={12} className="setup-screen-header">
+                <Image className="setup-screen-title" src={title} fluid />
             </Col>
-            <Col xs={12} sm={7} lg={12} className='d-flex flex-column justify-content-start align-items-center p-1'
-                style={{ height: '40%', maxHeight: '40%' }}>
-                <div style={{ width: '80%' }}>
-                    <h2 style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>Select a level</h2>
-                    <ListGroup variant="flush" as="ol" numbered
-                        style={{ width: '100%', height: '150px', maxHeight: '150px', overflowY: 'auto' }}>
-                        <ListGroup.Item
-                            action
-                            as="li"
-                            className="d-flex justify-content-between align-items-start">
-                            <div className="ms-2 me-auto">
-                                <div className="fw-bold">Subheading</div>
-                                Cras justo odio
-                            </div>
-                            <Badge bg="primary" pill>
-                                14
-                            </Badge>
-                        </ListGroup.Item>
+            <Col xs={12} sm={7} lg={12} className="setup-screen-content">
+                <div className="setup-screen-levels">
+                    <h2 className="setup-screen-heading">Select level</h2>
+                    <ListGroup variant="flush" as="ol" className="setup-screen-list">
+                        {levels
+                            .sort((a, b) => a.estimatedDifficulty - b.estimatedDifficulty)
+                            .reverse()
+                            .map((level, index) => {
+                                return <ListGroup.Item
+                                    key={index}
+                                    action
+                                    onPointerDown={() => {
+                                        setSelectedLevelIndex(index)
+                                    }}
+                                    active={selectedLevelIndex === index}
+                                    as="li"
+                                    className={"d-flex justify-content-between align-items-start ".concat(selectedLevelIndex === index ? "selected" : "")}>
+                                    <div className="ms-2 me-auto">
+                                        <h5 className="level-title">{levels.length - index} . {level.levelName}</h5>
+                                        Cras justo odio
+                                    </div>
+                                    <Badge bg="primary" pill>
+                                        {level.estimatedDifficulty}
+                                    </Badge>
+                                </ListGroup.Item>
+                            })}
                     </ListGroup>
-                    <Row className='g-0 justify-content-between' style={{ width: '100%' }}>
-                        {/* <div className={classes} style={{ textAlign: 'center', height: '20%', backgroundColor: 'blue' }} onClick={() => props.onStartButton(levelConfig)} />
-            <div className={classes} style={{ textAlign: 'center', height: '20%', backgroundColor: 'red' }} onClick={() => props.onLevelEditorButton()} /> */}
-
-                        <Col xs={'auto'}>
-                            <Button variant="info" style={{ width: '100%' }}>Edit</Button>
+                    <Row className="setup-screen-buttons g-0">
+                        <Col xs={3}>
+                            <Button variant="info" className="setup-screen-button"
+                                onClick={() => props.onLevelEditorButton()}>Create
+                                <i className="bi bi-pencil-fill ms-2"></i>
+                            </Button>
                         </Col>
-                        <Col xs={'auto'} className='ms-2'>
-                            <Button variant="info" style={{ width: '100%' }}>Info</Button>
+                        <Col xs={3} className="ps-2">
+                            <Button variant="info" className="setup-screen-button">Info
+                                <i className="bi bi-info-circle-fill ms-2"></i>
+                            </Button>
                         </Col>
-                        <Col xs className='ms-2'>
-                            <Button variant="primary" style={{ width: '100%' }}>Primary</Button>
+                        <Col xs={6} className="ps-2">
+                            <Button variant="primary"
+                                className="setup-screen-button"
+                                onClick={() => props.onStartButton(levels[selectedLevelIndex])}>Play
+                                <i className="bi bi-play-fill ms-2"></i>
+                            </Button>
                         </Col>
                     </Row>
                 </div>
